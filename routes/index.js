77b2c41b155368e40a521792a61c4ecb220c7218
Alan Cardero <alan.cardero@gmail.com>
2018-06-30 12:08:42 -0400

    Mail sender example with templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
+const templates = require('../templates/templates');
+
 const express = require('express');
 const router  = express.Router();
+const nodemailer = require('nodemailer');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('email');
+});
+
+router.post('/send-email', (req, res, next) => {
+  const { email, subject, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAILACC,
+      pass: process.env.EMAILPASS,
+    }
+  });
+  transporter.sendMail({
+    from: 'alan.cadero@gmail.com',
+    to: email, 
+    subject: subject, 
+    html: templates.templateExample2(message),
+  })
+  .then(info => res.render('message', {email, subject, message, info}))
+  .catch(error => console.log(error));
 });
 
 module.exports = router;
